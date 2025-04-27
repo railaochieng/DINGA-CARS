92ad51d1ab1c8902d0155be08c7a1842c3c9d40b
dingacars <ochiengraila001@gmail.com>
2025-04-26 19:26:34 -0700

    add

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const totalCars = 29; // Total car images
+const totalCars = 39; // Total car images
 const carGallery = document.getElementById("carGallery");
 
 document.getElementById("viewCarsBtn").addEventListener("click", () => {
